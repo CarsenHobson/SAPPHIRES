f74c8949a87e0ed359658af388f17a2ab76c6811
CarseHobson <138251535+CarseHobson@users.noreply.github.com>
2024-02-15 14:03:01 -0700

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -4,11 +4,22 @@ from sps30 import SPS30
 
 # File path for baseline value
 BASELINE_FILE_PATH = "baseline_value.json"
-
+BASELINE_DATA_FILE = "Baselinedata.json"
 # Function to setup the SPS30 sensor
 def setup_sps30():
     sps.start_measurement()
     time.sleep(2)
+
+def log_data(pm2_5):
+    try:
+        with open(LOG_FILE_PATH, "a") as json_file:
+            entry_with_timestamp_and_key = {
+                "timestamp": int(time.time()),  # Add UNIX timestamp
+                "pm2_5": pm2_5,
+            }
+            json_file.write(json.dumps(entry_with_timestamp_and_key) + "\n")
+    except Exception as e:
+        print(f"Error writing to JSON file: {str(e)}")
 
 # Function to perform baseline measurement and update the file
 def update_baseline_value(baseline_duration=60):  # Set the baseline duration in seconds
@@ -21,10 +32,11 @@ def update_baseline_value(baseline_duration=60):  # Set the baseline duration in
     while time.time() - start_time < baseline_duration:
         sps.read_measured_values()
         data = sps.dict_values['pm2p5']
+        log_data(data)
         if data:
             baseline_data.append(data)
         time.sleep(1)
-
+    
     baseline_pm25 = sum(baseline_data) / len(baseline_data)
 
     print(f"Baseline PM2.5: {baseline_pm25} µg/m³")
