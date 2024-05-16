6cf9837a56239f9ae901434b0cd3ac83d020fd37
CarseHobson <138251535+CarseHobson@users.noreply.github.com>
2024-05-16 09:23:55 -0600

    Update ZeroW.py

--- ZeroW.py
+++ ZeroW.py
@@ -2,6 +2,7 @@ import time
 import paho.mqtt.client as mqtt
 import Adafruit_BME280
 from sps30 import SPS30
+import subprocess
 
 mqtt_username = "SAPPHIRE"
 mqtt_password = "SAPPHIRE"
@@ -16,15 +17,27 @@ client.username_pw_set(mqtt_username, mqtt_password)
 client.on_publish = on_publish
 client.connect(broker_address, 1883, 60)
 
-
 bme280 = Adafruit_BME280.BME280(address=0x77)
-
 sps30 = SPS30(port=1)
-
 
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
 
+def get_wifi_strength():
+    # Execute the iwconfig command to get WiFi information
+    result = subprocess.run(['iwconfig'], capture_output=True, text=True)
+    output_lines = result.stdout.split('\n')
+
+    # Find the line containing the signal strength
+    for line in output_lines:
+        if 'Signal level=' in line:
+            # Extract the signal strength value
+            signal_strength = line.split('Signal level=')[-1].split(' ')[0]
+            # Convert signal strength to dBm
+            signal_strength_dbm = int(signal_strength.replace('dBm', ''))
+            return signal_strength_dbm
+
+    return None
 
 try:
     sps30.read_measured_values()
@@ -40,10 +53,17 @@ try:
         "Humidity (%)": humidity,
     }
 
+    # Add WiFi Strength to sensor data
+    wifi_strength = get_wifi_strength()
+    if wifi_strength is not None:
+        sensor_data["WiFi Strength (dBm)"] = wifi_strength
+    else:
+        print("Unable to retrieve WiFi strength.")
+
     client.publish(mqtt_topic, str(sensor_data), qos=1)
     print(sensor_data)
     
-
 except KeyboardInterrupt:
     sps.stop_measurement()
     print("\nKeyboard interrupt detected. SPS30 and BME280 turned off.")
+
